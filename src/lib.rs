a2b85e53eca2e32b4dcdafb16353bdf4b33499fb
Mendelt Siebenga <msiebenga@gmail.com>
2020-07-13 23:19:40 +0200

    start with spi stub

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 
+use embedded_hal::blocking::spi::Write;
 use std::io;
 
 #[derive(PartialEq, Clone, Debug)]
@@ -10,6 +11,16 @@ pub enum TestError {
 impl From<io::Error> for TestError {
     fn from(e: io::Error) -> Self {
         TestError::Io(e.kind())
+    }
+}
+
+pub struct SpiStub {}
+
+impl Write<u8> for SpiStub {
+    type Error = TestError;
+
+    fn try_write(&mut self, _: &[u8]) -> Result<(), Self::Error> {
+        Ok(())
     }
 }
 
