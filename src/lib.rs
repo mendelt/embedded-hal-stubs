3df20e39aea4be605fec8d9dfbd21087e263bac5
Mendelt Siebenga <msiebenga@gmail.com>
2020-07-13 23:17:48 +0200

    some kind of error type might come in handy

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,18 @@
+
+use std::io;
+
+#[derive(PartialEq, Clone, Debug)]
+pub enum TestError {
+    /// An I/O-Error occurred
+    Io(io::ErrorKind),
+}
+
+impl From<io::Error> for TestError {
+    fn from(e: io::Error) -> Self {
+        TestError::Io(e.kind())
+    }
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
