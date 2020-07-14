81dd92b550d627c1d79b8fb7bb0e28a1a56595aa
Mendelt Siebenga <msiebenga@gmail.com>
2020-07-14 23:49:08 +0200

    add very simple programmable behavior to stub

--- src/lib.rs
+++ src/lib.rs
@@ -2,18 +2,30 @@ use embedded_hal::blocking::spi::Write;
 
 #[derive(PartialEq, Clone, Debug)]
 pub enum TestError {
-    StubbedError  // An error was raised as part of a test
+    StubbedError, // An error was raised as part of a test
 }
 
+pub struct SpiStub {
+    write_result: (u8, Result<(), TestError>),
 }
 
-pub struct SpiStub {}
+impl SpiStub {
+    pub fn new() -> Self {
+        SpiStub {
+            write_result: (0u8, Ok(())),
+        }
+    }
+
+    pub fn on_try_write(&mut self, buffer: u8, result: Result<(), TestError>) {
+        self.write_result = (buffer, result)
+    }
+}
 
 impl Write<u8> for SpiStub {
     type Error = TestError;
 
     fn try_write(&mut self, _: &[u8]) -> Result<(), Self::Error> {
-        Ok(())
+        self.write_result.1.clone()
     }
 }
 
@@ -23,7 +35,7 @@ mod tests {
 
     #[test]
     fn it_works() {
-        let mut stub = SpiStub {};
+        let mut stub = SpiStub::new();
         assert_eq!(stub.try_write(&[8u8, 7u8, 6u8]), Ok(()));
     }
 }
