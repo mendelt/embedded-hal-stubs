4880e98e9d3d56d1a48a2e6de9ce9c103a14e501
Mendelt Siebenga <msiebenga@gmail.com>
2020-07-15 22:55:55 +0200

    some fixes

--- src/lib.rs
+++ src/lib.rs
@@ -25,7 +25,7 @@ impl Write<u8> for SpiStub {
     type Error = TestError;
 
     fn try_write(&mut self, _: &[u8]) -> Result<(), Self::Error> {
-        self.write_result.1.clone()
+        self.write_result.clone()
     }
 }
 
@@ -42,7 +42,7 @@ mod tests {
     #[test]
     fn should_return_error() {
         let mut stub = SpiStub::new();
-        stub.on_try_write(8u8, Err(TestError::StubbedError));
+        stub.on_try_write(Err(TestError::StubbedError));
         assert_eq!(stub.try_write(&[8u8, 7u8, 6u8]), Err(TestError::StubbedError));
 
     }
