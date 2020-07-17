5403df43ecfa29e4801277692b53c93da4973bdb
Mendelt Siebenga <msiebenga@gmail.com>
2020-07-17 17:03:04 +0200

    start fluent interface for specifying return behaviour of functions

--- src/lib.rs
+++ src/lib.rs
@@ -5,21 +5,47 @@ pub enum TestError {
     StubbedError, // An error was raised as part of a test
 }
 
+pub struct Returns<T> {
+    return_values: Vec<T>
+}
+
+impl<T: Clone> Returns<T> {
+    fn values() -> Self {
+        Returns {
+            return_values: Vec::new()
+        }
+    }
+
+    fn once(mut self, value: T) -> Self {
+        self.return_values.push(value);
+        self
+    }
+
+    fn get_by_params(&mut self) -> T {
+        // Todo, look up values by parameters of call
+        
+        let (head, tail) = self.return_values.split_first().unwrap();
+        let result = head.clone();
+        self.return_values = tail.to_vec();
+        result
+    }
+}
+
 pub struct SpiStub {
-    write_result: Result<(), TestError>,
+    write_result: Returns<Result<(), TestError>>,
     write_iter_result: Result<(), TestError>,
 }
 
 impl SpiStub {
     pub fn arrange() -> Self {
         SpiStub {
-            write_result: Ok(()),
+            write_result: Returns::values(),
             write_iter_result: Ok(()),
         }
     }
 
-    pub fn try_write(mut self, result: Result<(), TestError>) -> Self {
-        self.write_result = result;
+    pub fn try_write(mut self, values: Returns<Result<(), TestError>>) -> Self {
+        self.write_result = values;
         self
     }
 
@@ -37,7 +63,7 @@ impl SpiStub {
 }
 
 pub struct SpiStubImpl {
-    write_result: Result<(), TestError>,
+    write_result: Returns<Result<(), TestError>>,
     write_iter_result: Result<(), TestError>,
 }
 
@@ -45,7 +71,7 @@ impl Write<u8> for SpiStubImpl {
     type Error = TestError;
 
     fn try_write(&mut self, _: &[u8]) -> Result<(), Self::Error> {
-        self.write_result.clone()
+        self.write_result.get_by_params()
     }
 }
 
@@ -81,7 +107,7 @@ mod tests {
     #[test]
     fn should_return_error_on_try_write() {
         let mut stub = SpiStub::arrange()
-            .try_write(Err(TestError::StubbedError)).go();
+            .try_write(Returns::values().once(Err(TestError::StubbedError))).go();
 
         assert_eq!(
             stub.try_write(&[8u8, 7u8, 6u8]),
