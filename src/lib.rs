8c83cfceaeab2ba577703975a7c98a2ca37e5ec3
Mendelt Siebenga <msiebenga@gmail.com>
2020-07-21 23:00:05 +0200

    move programmable returns to returns module

--- src/lib.rs
+++ src/lib.rs
@@ -1,58 +1,10 @@
 use embedded_hal::blocking::spi::{Transfer, Write, WriteIter};
 
 mod error;
+mod returns;
 
 use crate::error::TestError;
-
-#[derive(Clone)]
-pub enum Return<T> {
-    Once(T),
-    Always(T),
-}
-
-/// Stores the set of return values for a stubbed method where T is the return type and implements
-/// the fluent interface for specifying the return values
-pub struct Returns<T> {
-    return_values: Vec<Return<T>>,
-}
-
-pub fn returns<T>() -> Returns<T> {
-    Returns::default()
-}
-
-impl<T> Returns<T> {
-    pub fn once(mut self, value: T) -> Self {
-        self.return_values.push(Return::Once(value));
-        self
-    }
-
-    pub fn always(mut self, value: T) -> Self {
-        self.return_values.push(Return::Always(value));
-        self
-    }
-}
-
-impl<T: Clone> Returns<T> {
-    pub fn get_by_params(&mut self) -> T {
-        match self.return_values.split_first() {
-            Some((Return::Always(value), _)) => value.clone(),
-            Some((Return::Once(value), tail)) => {
-                let result = value.clone();
-                self.return_values = tail.to_vec();
-                result
-            }
-            _ => panic!("No expected result available"),
-        }
-    }
-}
-
-impl<T> Default for Returns<T> {
-    fn default() -> Self {
-        Returns {
-            return_values: Vec::new(),
-        }
-    }
-}
+use returns::{returns, Returns};
 
 pub struct SpiStub {
     write_result: Returns<Result<(), TestError>>,
