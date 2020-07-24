7d86c0370e0b8e3693d43ec193421e65e3ccbe93
Mendelt Siebenga <msiebenga@gmail.com>
2020-07-24 13:19:28 +0200

    move integration tests to tests module

---
+++ tests/spi.rs
@@ -0,0 +1,36 @@
+use embedded_hal_stubs::blocking::spi::SpiStub;
+use embedded_hal_stubs::error::TestError;
+use embedded_hal_stubs::returns::returns;
+use embedded_hal::blocking::spi::{WriteIter, Write};
+
+#[test]
+fn should_return_default_result_for_try_write() {
+    let mut stub = SpiStub::arrange().go();
+    assert_eq!(stub.try_write(&[8u8, 7u8, 6u8]), Ok(()));
+}
+
+#[test]
+fn should_arrange_results_for_try_write() {
+    let mut stub = SpiStub::arrange()
+        .try_write(returns(Err(TestError::StubbedError)).once())
+        .go();
+    assert_eq!(stub.try_write(&[]), Err(TestError::StubbedError));
+}
+
+#[test]
+fn should_return_default_result_for_try_write_iter() {
+    let mut stub = SpiStub::arrange().go();
+    assert_eq!(stub.try_write_iter(vec![]), Ok(()));
+}
+
+#[test]
+fn should_arrange_results_for_try_write_iter() {
+    let mut stub = SpiStub::arrange()
+        .try_write_iter(returns(Err(TestError::StubbedError)).once())
+        .go();
+
+    assert_eq!(
+        stub.try_write_iter(vec![8u8, 7u8, 6u8]),
+        Err(TestError::StubbedError)
+    );
+}
