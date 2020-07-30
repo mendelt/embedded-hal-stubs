f469982033197fd49c209c38148b6b41ed2e56a3
Mendelt Siebenga <msiebenga@gmail.com>
2020-07-30 22:02:16 +0200

    fix documentation for error.rs module

--- src/error.rs
+++ src/error.rs
@@ -1,5 +1,8 @@
+//! This module contains the error types for the embedded-hal-stubs library
+
+/// Error type that is returned by stubbed methods
 #[derive(PartialEq, Clone, Debug)]
 pub enum TestError {
-    // This error can be raised as part of a test
+    /// This error can be raised as part of a test
     StubbedError,
 }
